0c0b11bda5823de76313ac290899bbed7fade9eb
EricCasey <ericcasey509@gmail.com>
2016-08-21 15:10:23 -0400

    Leaned out code again
    added new IDs and an online area

--- casey/maincore.js
+++ casey/maincore.js
@@ -56,25 +56,6 @@ window.addEventListener('scroll', function(){ // on page scroll
 
 
 
-  //// Clock
-
-function startTime() {
-    var today = new Date();
-    var h = today.getHours();
-    var m = today.getMinutes();
-    var s = today.getSeconds();
-    m = checkTime(m);
-    s = checkTime(s);
-    document.getElementById('txt').innerHTML =
-    h + ":" + m + " EST";
-    var t = setTimeout(startTime, 500);
-}
-function checkTime(i) {
-    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
-    return i;
-}
-
-
  /// redirects to mobile page if width is == ?
 
  <!--
